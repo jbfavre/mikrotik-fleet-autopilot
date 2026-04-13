a22d6e8082e69f899ef072d2d01ee496a6f005ed
Jean Baptiste Favre <github@jbfavre.org>
2026-04-13 11:08:40 +0200

    feat(cmd/updates) Ensure subcommands alignement

--- cmd/export/export.go
+++ cmd/export/export.go
@@ -70,6 +70,7 @@ var Command = []*cli.Command{
 				displayStepCallback := display.NewStepCallback(line)
 				filename, err := export(ctx, host, "", exportCfg, deps, displayStepCallback) // Empty preferred filename = derive automatically
 				if err != nil {
+					line.CompleteStep("❌")
 					line.FinishError(err.Error())
 					lastErr = err
 					// Continue with other hosts even if one fails
